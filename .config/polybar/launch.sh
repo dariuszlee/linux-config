36a25a721e355c1ed5a981ffa4fdad3e5dfbf288
Darius Lee <dariuszlee93@gmail.com>
2019-09-30 10:47:35 +0200

    i3/polybar update and quickmarks and screen management

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,7 +10,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 export WIRED_CARD=$(nmcli | awk '/ethernet/ {print $1}' | head -1)
 export WIRELESS_CARD=$(nmcli | awk '/wifi/ {print $1}' | head -1)
-export CONNECTED_CARD=$(nmcli device status | grep ' connected ' | grep 'ethernet\|wifi' | cut -d' ' -f1)
+export CONNECTED_CARD=$(nmcli device status | grep ' connected ' | grep 'ethernet\|wifi' | cut -d' ' -f1 | head -n 1)
 
 # Launch bar1 and bar2
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
